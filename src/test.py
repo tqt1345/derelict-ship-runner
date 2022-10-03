fe12b706fd0436633fc9abb62d9b72f5a73f6896
tqt1345 <tqt1345@gmail.com>
2022-10-03 17:40:29 +1100

    Update test.py

--- src/test.py
+++ src/test.py
@@ -10,18 +10,23 @@ e.run_combat(player,monster)
 
 def test(player,monster):
     
+    if not monster.isAlive:
+        print(f"The {monster.type} is already dead")
+        return
+    
     inCombat = True
-
     while inCombat:
-        if not monster.isAlive:
-            print(f"The {monster.type} is already dead")
-            return
-        
         inCombat = e.healthCheck(player,monster)
         e.combatSequence(player,monster,'player')
         m.continue_prompt()
         
-        
+        inCombat = e.healthCheck(player,monster)
         e.combatSequence(player,monster,'monster')
         m.continue_prompt()
         
+    if not player.isAlive:
+        print(f"You have died")
+    elif not monster.isAlive:
+        print(f"You have won")
+        
+test(player,monster)
