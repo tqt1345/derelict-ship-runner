9d2e7e180f6c7f8bf04abf2a8bdb8c3f091f6ff5
tqt1345 <tqt1345@gmail.com>
2022-10-03 15:56:32 +1100

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,8 +1,27 @@
 import entities as e
+import menu as m
+from src.entities import combatSequence 
 
-player1 = e.Player()
-monster1 = e.Crawler()
+player = e.Player()
+monster = e.Crawler()
 
-e.run_combat(player1,monster1)
+e.run_combat(player,monster)
 
-#print(monster1.attack())
+
+def test(player,monster):
+    
+    inCombat = True
+
+    while inCombat:
+        if not monster.isAlive:
+            print(f"The {monster.type} is already dead")
+            return
+        
+        inCombat = healthCheck(player,monster)
+        e.combatSequence(player,monster,'player')
+        m.continue_prompt()
+        
+        
+        e.combatSequence(player,monster,'monster')
+        m.continue_prompt()
+        
