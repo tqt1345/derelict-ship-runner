f8edc255a2f250b0e88da6760e7b2b2e1763c007
tqt1345 <tqt1345@gmail.com>
2022-12-24 15:44:27 +1100

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,9 +1,33 @@
-import entities as e
-import menu as m
-import system as s
 
-player = e.Player()
-monster = e.Crawler()
 
-#e.run_combat(player,monster)
+def mainMenu():
+    prompt = ("Enter a number:\n"
+              "    -1. Option one\n"
+              "    -2. Option two\n"
+              "=>:")
+    while True:
+        try:
+            selection = int(input(prompt))
+            if selection == 1:
+                subMenu()
+            elif selection == 2:
+                subMenu2()
+        except ValueError:
+            print("Enter 1-2")
+    exit()
 
+def subMenu():
+    pass
+
+def subSubMenu():
+    pass
+
+
+def subMenu2():
+    pass
+
+def subSubMenu2():
+    pass
+
+
+mainMenu()
